2721c07c12c550e3197e62ca2e019b6bd80ce79f
Motolani Akinyemi <motolaniakinyemi@gmail.com>
2022-03-30 21:43:44 +0100

    ...

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let clientName =  document.getElementById('client-name');
 let loveInterest = document.getElementById('love-interest');
 
-let loveScore();
+let loveScore;
 
+document.getElementById('calculate-btn').addEventListener('submit', function(e){
+    clientName.value.length
+   
+});
