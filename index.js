9ab31bdad6888ef18734cb3a91218b25b2f421d6
Motolani Akinyemi <motolaniakinyemi@gmail.com>
2022-03-31 22:58:14 +0100

    Done. No thanks to Math.random though.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 let clientName =  document.getElementById('client-name');
 let loveInterest = document.getElementById('love-interest');
+let link = document.getElementById('link');
+let result = document.getElementById('result');
 
 let loveScore;
 
-document.getElementById('calculate-btn').addEventListener('submit', function(e){
-    clientName.value.length
+document.getElementById('calculate-btn').addEventListener('click', function(e){
+    let x = clientName.value.length; 
+    let y = loveInterest.value.length;
+    let z = ((x + y)*5)/4 ; 
+
+
+window.location.href=`/results.html?percent=${z}`;
+
    
 });
+
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  result.innerHTML = `${params.percent}%`
